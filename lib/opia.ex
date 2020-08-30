b7aa48644e720f311e2ad3a6d726634ecac52a4e
Alisina Bahadori <alisina.bm@gmail.com>
2020-08-30 23:20:25 +0430

    Initial commit

---
+++ lib/opia.ex
@@ -0,0 +1,18 @@
+defmodule Opia do
+  @moduledoc """
+  Documentation for `Opia`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Opia.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/opia_test.exs
@@ -0,0 +1,8 @@
+defmodule OpiaTest do
+  use ExUnit.Case
+  doctest Opia
+
+  test "greets the world" do
+    assert Opia.hello() == :world
+  end
+end
